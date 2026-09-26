7debc1581d8a9ba87a6d41a3a73bec0d876bfb08
RayssaRR <rayssa_snt@hotmail.com>
2026-09-25 23:22:27 -0300

    alteração card-image

--- frontend/src/components/cards/card-image-product.tsx
+++ frontend/src/components/cards/card-image-product.tsx
@@ -22,8 +22,13 @@ export function CardImageProduct({ product }: CardProductProps) {
 
   const addItem = useCartStore((state) => state.addItem);
 
+  const artisanName =
+    typeof product.artisan === "object" && product.artisan !== null
+      ? product.artisan.name
+      : product.artisan || "Acervo Manoa";
+
   return (
-    <Card className="mx-auto w-full max-w-sm pt-0 bg-(--barro-claro) cursor-pointer hover:scale-103 transition-transform">
+    <Card className="mx-auto w-full max-w-sm pt-0 bg-(--barro-claro) cursor-pointer hover:scale-101 transition-transform">
       <div className="relative aspect-video w-full overflow-hidden">
         <span className="absolute top-3 left-3 z-10 bg-white text-(--barro) text-xs font-semibold px-3 py-1 rounded-full shadow-md">
           {product.category}
@@ -33,15 +38,19 @@ export function CardImageProduct({ product }: CardProductProps) {
         </span>
 
         <div className="absolute top-3 right-3 z-10 bg-white p-2 rounded-full">
-          <Heart></Heart>
+          <Heart />
         </div>
-        <img src={product.image} alt={product.name} className="object-cover" />
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-full h-full object-cover"
+        />
       </div>
       <CardHeader>
         <div className="flex justify-between h-7">
-          <p className="text-(--mangue)">{product.artisan || "Acervo Manoa"}</p>
+          <p className="text-(--mangue)">{artisanName}</p>
           <div className="flex justify-center items-center gap-1">
-            <Star className="fill-(--sol) text-(--sol) size-4"></Star>
+            <Star className="fill-(--sol) text-(--sol) size-4" />
             <p className="text-(--sol) font-bold">{product.stars}</p>
           </div>
         </div>
@@ -53,9 +62,9 @@ export function CardImageProduct({ product }: CardProductProps) {
           {product.description}
         </CardDescription>
 
-        <hr className="my-2"></hr>
+        <hr className="my-2" />
         <div className="flex justify-between items-center">
-          <div className="">
+          <div>
             <p>Preço</p>
             <p className="text-(--carvao) text-2xl font-bold">
               {formattedPrice}
@@ -64,9 +73,12 @@ export function CardImageProduct({ product }: CardProductProps) {
           <Button
             className="py-2 px-8 cursor-pointer"
             size="sm"
-            onClick={() => addItem(product)}
+            onClick={(e) => {
+              e.stopPropagation();
+              addItem(product);
+            }}
           >
-            <ShoppingCartPlus></ShoppingCartPlus> Comprar
+            <ShoppingCartPlus /> Comprar
           </Button>
         </div>
       </CardHeader>
